ff02865cbb3311df1f0e9169feaa36af1f976785
kdlibiran <kglibiran@up.edu.ph>
2023-10-24 23:02:21 +0800

    Changed title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Inventory App",
+  description: "Free inventory app for small businesses",
 };
 
 export default function RootLayout({
